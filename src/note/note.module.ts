62ac1ba6c979ceec8da1155e4a08f0fd1ebb9feb
andisiambuku <roselandambuku@gmail.com>
2023-08-29 19:24:55 +0300

    note module config

--- src/note/note.module.ts
+++ src/note/note.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NoteService } from './note.service';
 import { NoteResolver } from './note.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Note } from './entities/note.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Note])],
   providers: [NoteResolver, NoteService],
 })
 export class NoteModule {}
