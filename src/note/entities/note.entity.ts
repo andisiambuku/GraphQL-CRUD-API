dbf0c4eb7174528c15ca1d96d2b1f9f9a3d6de62
andisiambuku <roselandambuku@gmail.com>
2023-08-29 19:23:51 +0300

    db columns

--- src/note/entities/note.entity.ts
+++ src/note/entities/note.entity.ts
@@ -1,13 +1,48 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+// import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
+// @Entity()
+// export class Note {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+
+//   @Column()
+//   title: string;
+
+//   @Column()
+//   body: string;
+// }
+// note.type.ts
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@ObjectType()
 @Entity()
 export class Note {
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Field()
+  @Column('text', { nullable: false })
   title: string;
 
-  @Column()
+  @Field()
+  @Column('text', { nullable: false })
   body: string;
+
+  @Field()
+  @Column()
+  @CreateDateColumn()
+  created_at: Date;
+
+  @Field()
+  @Column()
+  @UpdateDateColumn()
+  updated_at: Date;
 }
